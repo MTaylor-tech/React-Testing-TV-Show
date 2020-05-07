b9d49bfad0eaafde91402ebf54e8d49422d20eab
Matthew Taylor <majikpig@gmail.com>
2020-05-06 23:44:33 -0400

    more tests

--- src/tests/Episodes.test.js
+++ src/tests/Episodes.test.js
@@ -10,18 +10,15 @@ test("Episode list renders correctly with no episodes", () => {
   const { queryAllByTestId } = render(
     <Episodes episodes={[]} />
   );
-
-  expect(queryAllByTestId("episode")).toStrictEqual([]);
+  expect(queryAllByTestId("episode")).toStrictEqual([]); //should not display any episodes
   expect(queryAllByTestId("episode")).toHaveLength(0);
 });
 
 test("Episode list renders correctly with episodes", () => {
-
   const { queryAllByTestId, queryAllByText } = render(
     <Episodes episodes={formattedSeasons['Season 1']} />
   );
-
-  expect(queryAllByTestId("episode")).toHaveLength(8);
-  expect(queryAllByText(/chapter/i)).toHaveLength(8);
-  expect(queryAllByText(/60 minutes/i)).toHaveLength(8);
+  expect(queryAllByTestId("episode")).toHaveLength(8); //Season 1 has 8 episodes in the mock data
+  expect(queryAllByText(/chapter/i)).toHaveLength(8); //The word "Chapter" appears in each episode box
+  expect(queryAllByText(/60 minutes/i)).toHaveLength(8); //Each box also displays "60 Minutes" for the length
 });
